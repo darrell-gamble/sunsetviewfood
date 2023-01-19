9f91e4c704d9a99944475c161b35e86faab304bc
Darrell Gamble <darrell.gamble@ujuzitek.com>
2023-01-19 11:36:12 +0300

    minor style changes + syncfusion integration

--- Client/wwwroot/assets/js/scrollto.js
+++ Client/wwwroot/assets/js/scrollto.js
@@ -9,3 +9,23 @@ window.blazorHelpers = {
         }
     }
 };
+
+/* floating btn js */
+let unitsScrollTopBtn = document.getElementById("unitsBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    unitsScrollTopBtn.style.display = "block";
+  } else {
+    unitsScrollTopBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
