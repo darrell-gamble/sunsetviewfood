b60e669118f938b8885d61bde3c55ed7df34383f
Darrell Gamble <darrell.gamble@ujuzitek.com>
2023-01-26 23:30:04 +0300

    restores deployed html

--- Api/Booking.cs
+++ Api/Booking.cs
@@ -12,20 +12,20 @@ using Newtonsoft.Json;
 
 namespace SunsetHomes.API
 {
-    public class HttpTriggerWithOpenAPICSharp1
+    public class Booking
     {
-        private readonly ILogger<HttpTriggerWithOpenAPICSharp1> _logger;
+        private readonly ILogger<Booking> _logger;
 
-        public HttpTriggerWithOpenAPICSharp1(ILogger<HttpTriggerWithOpenAPICSharp1> log)
+        public Booking(ILogger<Booking> log)
         {
             _logger = log;
         }
 
-        [FunctionName("HttpTriggerWithOpenAPICSharp1")]
+        [FunctionName("Booking")]
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-        public async Task<IActionResult> Run(
+        public async Task<IActionResult> GetRooms(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
