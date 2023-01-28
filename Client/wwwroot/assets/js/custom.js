d1448647f7f54410f32c5de4f2e7a200252d8f63
Darrell Gamble <darrell.gamble@ujuzitek.com>
2023-01-28 15:04:34 +0300

    Branch checkin.

--- Client/wwwroot/assets/js/custom.js
+++ Client/wwwroot/assets/js/custom.js
@@ -388,3 +388,18 @@ $(document).ready(function(){
 // }
 
 
+// Scroll to top jquery code
+var btn = $('#backToTop');
+$(window).on('scroll', function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, '300');
+});
