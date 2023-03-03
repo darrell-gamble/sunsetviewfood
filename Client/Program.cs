d1e3c79b1567e07f7b85788b59107ce10c87c8a5
Darrell Gamble <darrell.gamble@ujuzitek.com>
2023-03-03 06:03:33 +0300

    Intermediary style commit.  Removes unfinished tabs.  Booking tab sends email

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
 using Syncfusion.Blazor;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,4 +12,10 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NzkzNDc4QDMyMzAyZTM0MmUzMGM1ZG1WT2N6ZHBaSGNWQzhEalpKTFFqNlhkZjMxTC9ncVN2WHhLUUMwalU9;NzkzNDc5QDMyMzAyZTM0MmUzMGJnNXdhSml4dDZxSW1WU1h6OTkzc2tWSjl5UEtPcDJhYm00ZHRoVW16dzQ9");
 builder.Services.AddSyncfusionBlazor();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+builder.Services.AddScoped<GoogleMapClickEventArgs>();
+builder.Services.AddScoped<GoogleMapPosition>();
 await builder.Build().RunAsync();
